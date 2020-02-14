24081daf2efeff7446e786fcfcc0d0365cdf7d53
Jamie R. Oaks <joaks1@gmail.com>
2020-02-13 20:06:54 -0600

    Adding shell script for cleaning up sim output.

---
+++ bin/tar-sim-files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+bin_dir="$(pwd)"
+
+for sim_dir in ../ecoevolity-simulations/*/batch001
+do
+    cd "$sim_dir"
+    if [ -e "simcoevolity-model-used-for-sims.yml" ]
+    then
+        pwd
+        rm *.nex
+        rm *.sh
+        rm *.sh.o*
+        rm *simcoevolity-sim-*yml
+        tar czf sim-files-run-1.tar.gz run-1-* && rm run-1-*
+        tar czf sim-files-run-2.tar.gz run-2-* && rm run-2-*
+        tar czf sim-files-run-3.tar.gz run-3-* && rm run-3-*
+        tar czf sim-files-run-4.tar.gz run-4-* && rm run-4-*
+        tar czf sim-files-true-values.tar.gz simcoevolity-sim-???-true-values.txt simcoevolity-model-used-for-sims.yml && rm simcoevolity-sim-???-true-values.txt simcoevolity-model-used-for-sims.yml
+    else
+        echo "Skipping $(pwd)"
+    fi
+    cd "$bin_dir"
+done
+
+cd "$bin_dir"
